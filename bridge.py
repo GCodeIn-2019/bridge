e6ca054521e74d3a081c6c69abb9b36f86147b4e
scoopgracie <scoopgracie@gmail.com>
2020-01-28 08:48:16 -0800

    Remove need for passing in __name__

--- bridge.py
+++ bridge.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import json
 import importlib
+import inspect
 with open('modules.json', 'r') as f:
     modnames = json.load(f)
 
 platforms = [ importlib.import_module(module) for module in modnames ]
 
-def send(sending_platform, user, message):
+def send(user, message):
+
+    frm = inspect.stack()[1]
+    sending_platform = inspect.getmodule(frm[0]).__name__
     for platform in platforms:
         if platform.__name__ != sending_platform:
             platform.send(sending_platform, user, message)
